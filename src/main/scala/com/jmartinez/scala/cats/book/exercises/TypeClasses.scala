0009dc36d02ecaf92de326e699140492bab31907
Javier Martínez Jiménez <javier.martinez.jimenez@tecsisa.com>
2018-01-15 23:08:40 +0100

    Eq and Show type classes

--- src/main/scala/com/jmartinez/scala/cats/book/exercises/TypeClasses.scala
+++ src/main/scala/com/jmartinez/scala/cats/book/exercises/TypeClasses.scala
@@ -59,3 +59,36 @@ object PrintableInstances {
         s"${value.name} is a ${value.age} year-old ${value.color} cat"
     }
 }
+
+/**
+  * Implementation of the same functionality as above,
+  * but using [cats.Show] type class
+  */
+object showTypeClassInstances {
+
+  import cats.Show
+
+  implicit val catShow: Show[Cat] =
+    Show.show(cat => s"${cat.name} is a ${cat.age} year-old ${cat.color} cat")
+
+}
+
+/**
+  *
+  * Instances for [cats.Eq] Type class
+  *
+  */
+object eqTypeClassInstances {
+
+  import cats.Eq
+  import cats.instances.int._
+  import cats.instances.string._
+  import cats.syntax.eq._
+
+  implicit val catEq: Eq[Cat] =
+    Eq.instance { (cat1, cat2) =>
+      cat1.name === cat2.name &&
+      cat1.age === cat2.age &&
+      cat1.color === cat2.color
+    }
+}
--- src/test/scala/com/jmartinez/scala/cats/book/exercises/TypeClassesSpec.scala
+++ src/test/scala/com/jmartinez/scala/cats/book/exercises/TypeClassesSpec.scala
@@ -1,8 +1,13 @@
 package com.jmartinez.scala.cats.book.exercises
 
-import org.scalatest.{Matchers, WordSpec}
+import cats.kernel.Eq
+import cats.syntax.show._
+import cats.syntax.eq._
 import com.jmartinez.scala.cats.book.exercises.PrintableInstances._
 import com.jmartinez.scala.cats.book.exercises.PrintableSyntax._
+import com.jmartinez.scala.cats.book.exercises.eqTypeClassInstances._
+import com.jmartinez.scala.cats.book.exercises.showTypeClassInstances._
+import org.scalatest.{Matchers, WordSpec}
 
 /**
   * Simple testing to show how to use Printable type class
@@ -23,6 +28,28 @@ class TypeClassesSpec extends WordSpec with Matchers {
 
     "get string representation of a cat using interface syntax" in {
       jasperCat.format shouldBe stringRepresentationJasperCat
+    }
+  }
+
+  "Show type class" should {
+    "get string representation of a cat" in {
+      jasperCat.show shouldBe stringRepresentationJasperCat
+    }
+  }
+
+  "Eq type class" should {
+    "get true when the cats are the same, using eqv operator" in {
+      val jasperCat2 = Cat("Jasper", 10, "black")
+      Eq.eqv(jasperCat, jasperCat2) shouldBe true // not using === for ambiguity
+    }
+
+    "get false when the cats are different, using eqv operator" in {
+      val bellaCat = Cat("Bella", 10, "black")
+      Eq.eqv(jasperCat, bellaCat) shouldBe false // not using === for ambiguity
+    }
+
+    "get false when the cats are the same, using =!= operator" in {
+      jasperCat =!= jasperCat shouldBe false
     }
   }
 }
